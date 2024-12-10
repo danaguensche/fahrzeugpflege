2ac319cd402f27dd6f68e55bf6a9fb4a2f9674bd
DuBose <patrick.dubose@AUSBILDUNG.bbw-hof.de>
2024-12-10 10:36:19 +0100

    used alertbase component to generate confirmation alert for logout and changed arrow function notation of data in MenuItemsClosed and MenuItemsOpened to data() {return {}} notation due to arrow function's special rule for the this-keyword

--- resources/js/components/sidebar/MenuItemsOpened.vue
+++ resources/js/components/sidebar/MenuItemsOpened.vue
@@ -11,6 +11,7 @@
             </div>
         </div>
     </div>
+    <alert-base v-if="this.alertVisible" v-on="alertHandlers" alert-heading="Wirklich Abmelden?" alert-paragraph="Wollen Sie sich abmelden?" alert-close-button="Abbrechen" alert-okay-button="Abmelden" alert-type-class="alertTypeConfirmation"/>
 </template>
 
 
@@ -25,35 +26,46 @@ export default {
         MenuButton
     },
 
-    data: () => ({
+    data() {
+        return {
+            logoutClicked: false,
+            alertVisible: false,
+            alertHandlers: {
+                confirmationClicked: this.logout,
+                closeAlertClicked: this.updateAlertVisibility
+            },
+            //Text-Inhalte der Sidebar
+            menuitems: [
+                { id: 1, name: 'Dashboard' },
+                { id: 2, name: 'Kalender' },
+                { id: 3, name: 'Fahrzeuge' },
+                { id: 4, name: 'Kunden' },
+                { id: 5, name: 'Aufträge' },
+                { id: 6, name: 'Berichte' },
+                { id: 7, name: 'Profil' },
+                { id: 8, name: 'Einstellungen' },
+                { id: 9, name: 'Abmelden' },
+            ],
 
-        //Text-Inhalte der Sidebar
-        menuitems: [
-            { id: 1, name: 'Dashboard' },
-            { id: 2, name: 'Kalender' },
-            { id: 3, name: 'Fahrzeuge' },
-            { id: 4, name: 'Kunden' },
-            { id: 5, name: 'Aufträge' },
-            { id: 6, name: 'Berichte' },
-            { id: 7, name: 'Profil' },
-            { id: 8, name: 'Einstellungen' },
-            { id: 9, name: 'Abmelden' },
-        ],
-
-        //Bildnamen
-        iconPaths: [
-            { id: 1, name: new URL('@/img/sidebar-img/dashboard-icon.png', import.meta.url).href },
-            { id: 2, name: new URL('@/img/sidebar-img/calendar-icon.png', import.meta.url).href },
-            { id: 3, name: new URL('@/img/sidebar-img/cars-icon.png', import.meta.url).href },
-            { id: 4, name: new URL('@/img/sidebar-img/customer-icon.png', import.meta.url).href },
-            { id: 5, name: new URL('@/img/sidebar-img/jobs-icon.png', import.meta.url).href },
-            { id: 6, name: new URL('@/img/sidebar-img/reports-icon.png', import.meta.url).href },
-            { id: 7, name: new URL('@/img/sidebar-img/profile-icon.png', import.meta.url).href },
-            { id: 8, name: new URL('@/img/sidebar-img/settings-icon.png', import.meta.url).href },
-            { id: 9, name: new URL('@/img/sidebar-img/logout-icon.png', import.meta.url).href },
-        ]
-    }),
-
+            //Bildnamen
+            iconPaths: [
+                { id: 1, name: new URL('@/img/sidebar-img/dashboard-icon.png', import.meta.url).href },
+                { id: 2, name: new URL('@/img/sidebar-img/calendar-icon.png', import.meta.url).href },
+                { id: 3, name: new URL('@/img/sidebar-img/cars-icon.png', import.meta.url).href },
+                { id: 4, name: new URL('@/img/sidebar-img/customer-icon.png', import.meta.url).href },
+                { id: 5, name: new URL('@/img/sidebar-img/jobs-icon.png', import.meta.url).href },
+                { id: 6, name: new URL('@/img/sidebar-img/reports-icon.png', import.meta.url).href },
+                { id: 7, name: new URL('@/img/sidebar-img/profile-icon.png', import.meta.url).href },
+                { id: 8, name: new URL('@/img/sidebar-img/settings-icon.png', import.meta.url).href },
+                { id: 9, name: new URL('@/img/sidebar-img/logout-icon.png', import.meta.url).href },
+            ]
+        }
+    },
+    provide() {
+        return {
+            alertVisible: this.alertVisible
+        }
+    },
     methods: {
 
         //Formatiert Einträge aus dem menuitems Array und gibt die Route zu der dazugehörigen Seite zurück
@@ -83,14 +95,15 @@ export default {
 
         redirectToView(menuitem) {
             if (menuitem.name === 'Abmelden') {
-                this.logout();
+                this.alertVisible = true;
+                this.logoutClicked = true;
             } else {
                 this.$router.push(this.getRoute(menuitem.name));
             }
         },
 
         logout() {
-            if (confirm("Wollen Sie sich abmelden?") == true) {
+            this.alertVisible = false;
                 axios.post('/logout', {}, {
                     headers: {
                         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
@@ -102,7 +115,9 @@ export default {
                     console.error('Logout fehlgeschlagen', error);
                     alert('Logout fehlgeschlagen. Bitte versuchen Sie es erneut.');
                 });
-            }
+        },
+        updateAlertVisibility() {
+            this.alertVisible = !this.alertVisible;
         }
 
 
