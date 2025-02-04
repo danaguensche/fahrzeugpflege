68a6ea426637b7e055d70bdf35e21e60d6ea0b3c
DuBose <patrick.dubose@AUSBILDUNG.bbw-hof.de>
2025-02-04 11:45:08 +0100

    erste Dateien für Dokumentationswesen

---
+++ resources/js/components/Reports/Reporting.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Berichtsauswahl</h1>
+    <section class="report-list">
+        <a href="reportTypes/uebergabeprotokoll" class="report-link">Übergabeprotokoll</a>
+        <a href="reportTypes/kundenauftrag" class="report-link">Kundenauftrag</a>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+import { mapState } from 'vuex';
+
+export default {
+   
+}
+</script>
+
+<style scoped>
+    .report-list {
+        background-color: black;
+        border: 4px solid var(--primary-color-hover);
+        width: 100%;
+    }
+    .report-link {
+        background-color: var(--primary-color);
+    }
+    @media only screen and (max-width: 650px) {
+
+    }
+</style>
--- resources/js/components/pages/ReportsPage.vue
+++ resources/js/components/pages/ReportsPage.vue
@@ -1,15 +1,18 @@
 <template>
     <MainSidebar></MainSidebar>
+    <Reporting></Reporting>
 </template>
 
 <script>
 
 import MainSidebar from '../sidebar/MainSidebar.vue';
+import Reporting from '../Reports/Reporting.vue';
 
 export default {
     name: "ReportsPage",
     components: {
-        MainSidebar
+        MainSidebar,
+        Reporting
     }
 }
 
