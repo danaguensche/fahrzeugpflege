4328e7bc8e76a4dd3368e26a6c3342c5d2f13854
DuBose <patrick.dubose@AUSBILDUNG.bbw-hof.de>
2025-02-25 15:17:21 +0100

    basic framework for the individual reports forms

--- resources/js/components/Reports/FormUebergabeprotokoll.vue
+++ resources/js/components/Reports/FormUebergabeprotokoll.vue
@@ -1,18 +1,45 @@
 <template>
 <form></form>
+<!--v-model="itemNumber"-->
+<ReportTextEntry rteLabel="Bob" rteKey="bobby"></ReportTextEntry>
 </template>
 
 <script>
 import axios from 'axios';
 import { mapState } from 'vuex';
+import ReportTextEntry from './FormComponents/ReportTextEntry.vue';
 
 export default {
-   computed: {
+    name: "FormUebergabeprotokoll",
+    components: {
+        ReportTextEntry
+    },
+    inject: {
+        formValues: {
+            from: 'selectedEntry',
+            default: []
+        }
+        /*
+            All the different form input elements inside <template> will be given v-model to be bound to corresponding
+            values in the formValues associative array. When formValues changes, it will automatically change the affected inputs, and the other
+            way around.
+        */
+    },
+    computed: {
         ...mapState(['isSidebarOpen'])
-    }
+    },
+    data(){
+        return {
+            
+        }
+    },
 }
 </script>
 
 <style scoped>
-
+    form {
+        background-color: blue;
+        height: 50svh;
+        width: 50svw;
+    }
 </style>
