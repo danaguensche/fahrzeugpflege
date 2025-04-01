86dce3daf7e8190f125a0a78d3e0695cb7239f23
DuBose <patrick.dubose@AUSBILDUNG.bbw-hof.de>
2025-04-01 14:38:40 +0200

    model/factory/seeder files for new DB tables
    Will start filling the models and corresponding
    factories/seeders next week

---
+++ app/Models/ServicePricing.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServicePricing extends Model
+{
+    /** @use HasFactory<\Database\Factories\ServicePricingFactory> */
+    use HasFactory;
+}
