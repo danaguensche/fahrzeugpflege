e45fe472c384f1b2818858e0bb3a258b08b28449
DuBose <patrick.dubose@AUSBILDUNG.bbw-hof.de>
2025-04-01 14:38:40 +0200

    model/factory/seeder files for new DB tables
    Will start filling the models and corresponding
    factories/seeders next week

---
+++ app/Models/ExtraCharge.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExtraCharge extends Model
+{
+    /** @use HasFactory<\Database\Factories\ExtraChargeFactory> */
+    use HasFactory;
+}
