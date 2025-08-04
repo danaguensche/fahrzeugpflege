7dc48e559a6f1b0bb2282d1f94367ef626983a37
dana.guensche <dana.guensche@ausbildung.diakonie-am-campus.de>
2025-08-04 11:37:09 +0200

    ui improvments

--- resources/js/components/Cars/CarDetails/Page.vue
+++ resources/js/components/Cars/CarDetails/Page.vue
@@ -8,24 +8,15 @@
 <script>
 import { mapState } from 'vuex';
 import Details from './Details.vue';
-import BreadCrumbs from '../../CommonSlots/BreadCrumbs.vue';
 
 export default {
     components: {
         Details,
-        BreadCrumbs
     },
     computed: {
         ...mapState(['isSidebarOpen'])
     },
     data() {
-        return {
-            customLinks: [
-                { title: 'Dashboard', href: '/dashboard', disabled: false },
-                { title: 'Fahrzeuge', href: '/fahrzeuge', disabled: false },
-                { title: 'Details', href: '/fahrzeuge/fahrzeugdetails', disabled: true },
-            ]
-        };
     },
 }
 </script>
